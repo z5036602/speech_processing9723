ccf637015d97e590387515ef5a85c584455d2edc
z5036602 <joshua19970320@gmail.com>
2019-09-20 12:28:18 +1000

    week1

--- lab1/joshuaplot.m
+++ lab1/joshuaplot.m
@@ -1,12 +1,22 @@
 function joshuaplot(x,fs,B)
     freq_spec_unvoiced = abs(fftshift(fft(x)));
-    f = linspace(-fs/2,fs/2,256);
+    f = linspace(-fs/2,fs/2,length(x));
     figure;
     plot(f,log(freq_spec_unvoiced));
-    title(B)
+    C =  ' Frequency plot';
+    title(strcat(B,C));
+    xlabel('Frequency'), 
+    ylabel('Phase')
     xlim([0 fs/2]);
     
-
+    
+    figure; 
+    plot(f, angle(fftshift(fft(x))));
+    C = ' Phase plot';   
+    title(strcat(B,C)), 
+    xlabel('Frequency'), 
+    ylabel('Phase')
+    xlim([0 fs/2]);
 
 
 
--- lab1/lab1.m
+++ lab1/lab1.m
@@ -4,31 +4,42 @@ fs = 8000;
 %%
 figure(1);
 total_length = length(speech);
-%sound(speech,fs);
-fprintf('Total number of samples are: %i\n',total_length);
+sound(speech,fs);
+fprintf('Total number of samples in speech are: %i\n',total_length);
 plot(speech);
 title('Time domian speech signal');
 %%
 m = enframe(speech,256,256);
 unvoiced_frame = m(15,:);
+figure;
+plot(unvoiced_frame);
+title('unvoiced frame time domain signal')
 joshuaplot(unvoiced_frame,fs,'unvoiced');
 %%
-voiced_frame = m(6,:);
+voiced_frame = m(9,:);
+figure;
 plot(voiced_frame);
+title('voiced frame time domain signal')
 joshuaplot(voiced_frame,fs,'voiced');
 %%
-recObj = audiorecorder;
-disp('Start speaking.')
-recordblocking(recObj, 5);
-disp('End of Recording.');
+%recObj = audiorecorder;
+%disp('Start speaking.')
+%recordblocking(recObj, 5);
+%disp('End of Recording.');
 %%
 %play(recObj);
-y = getaudiodata(recObj);
+[y fs]= audioread('my_recording.wav');
 k = enframe(y,256,256);
 plot(y);
 %%
  voiced = k(66,:);
- unvoiced = k (145,:); 
+ unvoiced = k (118,:); 
+ figure;
+ plot(voiced);
+ title('voiced frame time domain signal')
+ figure;
+ plot(unvoiced);
+ title('unvoiced frame time domain signal')
 joshuaplot(unvoiced,fs,'unvoiced');
 joshuaplot(voiced,fs,'voiced');
 %%
